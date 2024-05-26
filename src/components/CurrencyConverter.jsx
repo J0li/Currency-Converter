074fcea420ea5528201cb80692ff061700f2f026
Oliveira_Ju <107575747+J0li@users.noreply.github.com>
2024-05-26 18:23:43 -0300

    Update CurrencyConverter.jsx

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -9,54 +9,54 @@ function CurrencyConverter() {
     const [result, setResult] = useState(0);
 
     useEffect(() => {
-        fetch('https://exchangeratesapi.io/latest')
+        fetch('http://api.exchangeratesapi.io/v1/latest?access_key = JvsBmUhGh423HkOFkJkfH8CzQes9khwZ')
+                .then(response => response.json())
+                .then(data => {
+                    setCoins([...Object.keys(data.rates)]);
+                })
+
+                .catch(error => console.error('Error searching for coins:', error));
+    }, []);
+
+    const currencyConverter = () => {
+        fetch('https://api.exchangeratesapi.io/v1/latest? access_key = JvsBmUhGh423HkOFkJkfH8CzQes9khwZ base=${ofCoin}&symbols=${forCoin}')
             .then(response => response.json())
             .then(data => {
-                setCoins([...Object.keys(data.rates)]);
+                const exchangeTax = data.rates[forCoin];
+                setResult(much * exchangeTax);
             })
+            .catch(error => console.error('Error searching for coins:', error));
+    };
 
-        .catch (error => console.error('Error searching for coins:', error));
-}, []);
-
-const currencyConverter = () => {
-    fetch('https://api.exchangeratesapi.io/latest?base=${ofCoin}&symbols=${forCoin}')
-        .then(response => response.json())
-        .then(data => {
-            const exchangeTax = data.rates[forCoin];
-            setResult(much * exchangeTax);
-        })
-        .catch (error => console.error('Error searching for coins:', error));
-};
-
-return (
-    <div>
-        <h2>Currency Converter</h2>
-        <div>
-            <label>To:</label>
-            <select value={ofCoin} onChange={(e) => setOfCoin(e.target.value)}>
-                {coins.map(coins =>(
-                <option key={coins} value={coins}>{coins}</option>
-        ))}
-            </select>
-            <input type="number" value={much} onChange={(e) =>
-                setMuch(e.target.value)} />
+    return (
+        <div className="exchange-box">
+            <h2>Currency Converter</h2>
+            <div>
+                <label>To:</label>
+                <select value={ofCoin} onChange={(e) => setOfCoin(e.target.value)}>
+                    {coins.map(coins => (
+                        <option key={coins} value={coins}>{coins}</option>
+                    ))}
+                </select>
+                <input type="number" value={much} onChange={(e) =>
+                    setMuch(e.target.value)} />
+            </div>
+            <div>
+                <label>From:</label>
+                <select value={forCoin} onChange={(e) =>
+                    setForCoin(e.target.value)}>
+                    {coins.map(coins => (
+                        <option key={coins} value={coins}>{coins}</option>
+                    ))}
+                </select>
+                <button className="btn" onClick={currencyConverter}>Convert</button>
+            </div>
+            <div>
+                <h3>Result:</h3>
+                <p>{result}</p>
+            </div>
         </div>
-        <div>
-            <label>From:</label>
-            <select value={forCoin} onChange={(e) =>
-                setForCoin(e.target.value)}>
-                {coins.map(coins => (
-                    <option key={coins} value={coins}>{coins}</option>
-                ))}
-            </select>
-            <button className="btn" onClick={currencyConverter}>Convert</button>
-        </div>
-        <div>
-            <h3>Result:</h3>
-            <p>{result}</p>
-        </div>
-    </div>
-);
+    );
 }
 
 export default CurrencyConverter;
